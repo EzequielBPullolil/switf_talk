cf0739f880c60188880573725d59ff5dc4d5f28c
EzequielK-source <programacionezequielk@gmail.com>
2023-05-25 13:29:45 -0300

    Changed the name of the event emitted - Changed the name of the event emitted in from update_message to new_message - The 'message' parameter of the send_message event was changed to 'data' that contains the chat id, the user id and the message - Added the docstring to the event

--- backend/src/modules/chat/event.py
+++ backend/src/modules/chat/event.py
@@ -3,6 +3,20 @@ from src import socketio
 
 
 @socketio.on('send_message')
-def handle_send_message(message):
-    print(message)
-    emit('update_messages', broadcast=True)
+def handle_send_message(data):
+    '''
+        Persists the received message and
+        emits the new_message event for all chat members
+
+        :data(dict): The event data
+            Must have the following keys:
+            * message(str): The message sended by the user
+            * chat_id(str): The id of the chat where the message will be persisted
+            * user_id(str): The id of the user who sends the message
+
+        emits: new_message 
+        raises: 
+            UnauthorizedUser: User not belongs to chat
+    '''
+    print(data['message'])
+    emit('new_message', room=data['chat_id'])
